195841df0bfaede3f3ebf1dfe283e827d3a0f305
mykolayudik <mykola.yudik.kn.2017@lpnu.ua>
2020-12-16 18:43:50 +0200

    laba4

---
+++ src/main/java/org/example/Square.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class Square {
+    private int expected;
+
+    public int getExpected() {
+        return expected;
+    }
+
+    public void equation() {
+        int n = 3;
+        int[][] a = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = n * i + j;
+            }
+        }
+
+        System.out.println("початкова матриця");
+        System.out.println("------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%4d", a[i][j]);
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+            }
+        }
+
+        System.out.println();
+        System.out.println("нова матриця");
+        System.out.println("------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%3d", a[i][j]);
+            }
+            System.out.println();
+        }
+        expected = a[1][0];
+    }
+}
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    Square square = new Square();
+
+    @Before
+    public void beforeTest() {
+        square.equation();
+    }
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        int expected = square.getExpected();
+        int actual = 1;
+        Assert.assertEquals(expected, actual);
+    }
+}
